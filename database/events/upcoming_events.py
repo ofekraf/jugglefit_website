5da104976af5f4255e2bdd696ae03a78098e2a92
Tamuz <tamuzvardi@gmail.com>
2025-03-01 18:33:10 +0200

    Add EJC to database

--- database/events/upcoming_events.py
+++ database/events/upcoming_events.py
@@ -3,7 +3,7 @@ from classes.event import UpcomingEvent
 # Keep ordered by date
 UPCOMING_EVENTS = [
     UpcomingEvent(
-        name="Israeli Juggling Convention 2025",
+        name="Israeli Juggling Convention 2025 (IJC)",
         date="16/04/2025",
         location="Gan Hashlosha, Israel",
         url="https://www.ijc.co.il/?lang=en",
@@ -15,7 +15,7 @@ UPCOMING_EVENTS = [
         ]
     ),
     UpcomingEvent(
-        name="Netherlands Juggling Festival 2025",
+        name="Netherlands Juggling Festival 2025 (NJF)",
         date="30/05/2025",
         location="Waalwijk, The Netherlands",
         url="https://nederlandsjongleerfestival.nl/en/",
@@ -27,5 +27,19 @@ UPCOMING_EVENTS = [
             "Rings Routes?",
             "Women's routes?"
         ]
-    )
+    ),
+    UpcomingEvent(
+        name="European Juggling Convention 2025 (EJC)",
+        date="To be determind",
+        location="Arnhem, The Netherlands",
+        url="https://ejc2025.org",
+        routes=[
+            "Balls Open",
+            "Clubs Open",
+            "Rings Open",
+            "Balls U18",
+            "Clubs U18",
+            "Rings U18",
+        ]
+    ),
 ]
