4d78fc7a4cc51885bb8518ec75fb118e92e803ad
Tamuz <tamuzvardi@gmail.com>
2025-03-04 18:31:23 +0200

    Fix some css as and remove debug flask

--- route_generator/tricks/base_trick.py
+++ route_generator/tricks/base_trick.py
@@ -4,9 +4,8 @@ from route_generator.tricks.tags import Tag
 
 @dataclass(kw_only=True)
 class Trick:
-    # def __init__(self, *, name: str, difficulty: int, props_count: int, tags: List[str], comment: Optional[str]=None):
     name: str
     props_count: int
-    difficulty: Optional[int] = None
+    difficulty: int = -1
     tags: Optional[Set[Tag]] = None
     comment: Optional[str] = None
