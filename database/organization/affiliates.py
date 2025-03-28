bbd30a071972555c84c12dfb0dc9a2829f58fb4e
tamuz_vardi <tamuz.vardi@upwind.io>
2025-03-29 00:36:18 +0300

    Add Ofek Snir as affiliate

--- database/organization/affiliates.py
+++ database/organization/affiliates.py
@@ -4,5 +4,6 @@ from classes.affiliate import Affiliate
 
 AFFILIATES = [
     Affiliate(name="Tamuz Vardi", residence="Israel", image_url="/static/images/Tamuz_Vardi.jpg"),
+    Affiliate(name="Ofek Snir", residence="Israel", image_url="/static/images/Ofek_Snir.jpg"),
     Affiliate(name="Ofek Rafaeli", residence="Israel", image_url="/static/images/Ofek_Rafaeli.jpg")
 ]
